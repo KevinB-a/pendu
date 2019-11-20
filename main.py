58aa58db3bcaeeff1d816dadda0a62238dfa0338
Kevin <kevin.billet.pro@gmail.com>
2019-11-20 11:10:57 +0100

    add commentary

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ from data import *
 
 username=name_choice()
 continue_game=True
-while continue_game==True:
+while continue_game== True:
 
-    word =random.choice(words_list)
+    word =random.choice(words_list) # choose random word in words_list
     list1=[]
     your_list=[]
 
     for el in word:
-        list1.append(el)
-        your_list.append('*')
+        list1.append(el) # add every letter in list1
+        your_list.append('*') # add * for every letter in word
 
-    print("".join(your_list))
+    print("".join(your_list)) # change your_list in str
 
-    nb_error = 0
+    nb_error = 0 # initialize number of error
     while nb_error < 8 :
-        letter = input("Enter your letter")
+        letter = input("Enter your letter").lower()
 
         if len(letter) > 1:
             if letter == word:
@@ -30,9 +30,9 @@ while continue_game==True:
                 break
 
         if letter in list1:
-            for (index,element) in enumerate(list1):
+            for index,element in enumerate(list1): 
                 if element==letter:
-                    your_list[index] = element
+                    your_list[index] = element # change * by letter if letter is in word
             good_letter = ''.join(your_list)
             print("good letter ",good_letter)
 
@@ -48,8 +48,8 @@ while continue_game==True:
 
     your_score=8-nb_error
     print("your score is",your_score)
-    rematch=input("do you want to play again ? enter yes for rematch and no for quit").lower()
-    if rematch =="no" or rematch == "n" or rematch =="quit" or rematch == "q" or rematch== "stop":
-        continue_game = False #if user choose stop the game continue_game become false and while loop stop
+    restart=input("do you want to play again ? enter yes for restart and no for quit").lower()
+    if restart =="yes" or rematch == "y" or rematch =="oui" or rematch == "o" :
+        continue_game = True #if user choose stop the game continue_game become false and while loop stop
     else:
-        continue_game = True #restart the game if user wants
+        continue_game = False #restart the game if user wants
