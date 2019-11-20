886526c69e1a462399f0cb14727c347fb43bb60b
Kevin <kevin.billet.pro@gmail.com>
2019-11-20 14:37:06 +0100

    create random_choice and number_of error functions

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import random
 
 import pickle
 
+from data import *
+
 def name_choice():
     """this function allow the player to choose enter a name or pseudo """
     username=""
@@ -10,23 +12,63 @@ def name_choice():
         return username
         print("welcome on pendu game ",username," !")
 
+def random_choice():
+    word=random.choice(words_list) # choose random word in words_list
+    return word
+
+def number_of_errors():
+    word_letter_list=[]
+    hidden_word=[]
+    for el in random_choice():
+        word_letter_list.append(el) # add every letter in list1
+        hidden_word.append('*') # add * for every letter in word
+    print("".join(hidden_word)) # change your_list in str
+    nb_error = 0 # initialize number of error
+    while nb_error < 8 :
+        letter = input("Enter your letter").lower() # if user enter upper letter convert to lower
+
+        if len(letter) > 1:
+            if letter == random_choice():
+                print("you find the word")
+                break
+
+        if letter in word_letter_list:
+            for index,element in enumerate(word_letter_list): # list index and element in list
+                if element==letter:
+                    hidden_word[index] = element # change * by letter if letter is in word
+            good_letter = ''.join(hidden_word) # convert hidden_word in str
+            print("good letter ",good_letter)
+
+            if word_letter_list==hidden_word:
+                print("you find the word")
+                break
+        else:
+            nb_error +=1
+            print('wrong letter,you still have %s try'%(8-nb_error))
+
+    if nb_error ==8:
+        print("you don't find the word")
+    your_score=8-nb_error
+    print("your score is",your_score)
+    return hidden_word ,your_score
+
 def collect_scores():
     """this function collect score of the player"""
 
-    if os.path.exists(name_file_scores): # if file exist 
+    if os.path.exists(name_file_scores): # if file exist
         # i collect the file
         file_scores = open(name_file_scores, "rb")
         collect_file = pickle.Unpickler(file_scores)
         scores = collect_file.load()
         fichier_scores.close()
-    else: # this file doesn't exist
+    else: # if file doesn't exist
         scores = {}
     return scores
 
 def save_scores(scores):
     """this function save score on score"""
 
-    file_scores = open(name_file_scores, "wb") #
+    file_scores = open(name_file_scores, "wb")
     save_file = pickle.Pickler(file_scores)
     save_file.dump(scores)
     fichier_scores.close()
--- main.py
+++ main.py
@@ -11,44 +11,10 @@ username=name_choice() #call function name_choice
 continue_game=True
 while continue_game== True:
 
-    word =random.choice(words_list) # choose random word in words_list
+    word = random_choice()
     word_letter_list=[]
     hidden_word=[]
-
-    for el in word:
-        word_letter_list.append(el) # add every letter in list1
-        hidden_word.append('*') # add * for every letter in word
-
-    print("".join(hidden_word)) # change your_list in str
-
-    nb_error = 0 # initialize number of error
-    while nb_error < 8 :
-        letter = input("Enter your letter").lower()
-
-        if len(letter) > 1:
-            if letter == word:
-                print("you find the word")
-                break
-
-        if letter in word_letter_list:
-            for index,element in enumerate(word_letter_list): # list index and element in list
-                if element==letter:
-                    hidden_word[index] = element # change * by letter if letter is in word
-            good_letter = ''.join(hidden_word)
-            print("good letter ",good_letter)
-
-            if word_letter_list==hidden_word:
-                print("you find the word")
-                break
-        else:
-            nb_error +=1
-            print('wrong letter,you still have %s try'%(8-nb_error))
-
-    if nb_error ==8:
-        print("you don't find the word")
-
-    your_score=8-nb_error
-    print("your score is",your_score)
+    number_of_errors()
     restart=input("do you want to play again ? enter yes for restart and no for quit").lower()
     if restart in ["yes","y","oui","o"] :
         continue_game = True #if user choose stop the game continue_game become false and while loop stop
